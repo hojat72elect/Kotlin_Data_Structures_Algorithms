704350e71402c4148c6ec41b2fbce28d0db8fe0c
mojan <hojat72elect@gmail.com>
2022-02-27 21:22:32 -0500

    There was a weird problem with Stack package, I deleted it and started implementing it in a langauge-independent way.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,13 @@
-fun main() {
+import linkedlist.LinkedList
 
+fun main() {
+    println(45 + 76)
+    val myLinkedList = LinkedList<Int>()
+    println(myLinkedList)
+    myLinkedList
+        .push(12)
+        .push(13)
+        .push(43)
+        .push(3)
+    println(myLinkedList)
 }
--- src/main/kotlin/stack/README.md
+++
@@ -1,3 +0,0 @@
-Remember that java already has a stack data structure implemented in "https://developer.android.com/reference/java/util/stack".
-
-I'm just implementing this class for educational purposes and to comply with the book contents.
