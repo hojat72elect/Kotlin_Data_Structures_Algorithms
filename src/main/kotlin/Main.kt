328e9df2aa649c3897f1ebec18e5257731b9529f
mojan <hojat72elect@gmail.com>
2022-03-16 14:59:49 -0400

    Some editions to HeapSort.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,8 @@
-import sort.heapSort
+import sort.heapify
 
 fun main() {
     val myList = arrayListOf(52, 23, 1, 6)
-    println("The original list: $myList")
-    heapSort(myList)
-    println("The sorted list: $myList")
+    for (i in 1 downTo 0) {
+        heapify(myList, 4, i)
+    }
 }
--- src/main/kotlin/sort/HeapSort.kt
+++ src/main/kotlin/sort/HeapSort.kt
@@ -7,7 +7,7 @@ package sort
  */
 
 fun <T : Comparable<T>> heapSort(inputList: MutableList<T>) {
-    // build a heap
+    // build a max-heap
     for (i in (inputList.size / 2 - 1) downTo 0) {
         heapify(inputList, inputList.size, i)
     }
@@ -15,7 +15,7 @@ fun <T : Comparable<T>> heapSort(inputList: MutableList<T>) {
     // replace it with the last element in the array.
 
     for (i in (inputList.size - 1) downTo 0) {
-        // arrA[0] is a root of the heap and is the max element
+        // inputList[0] is a root of the heap and is the max element
         // in the heap.
         inputList[0] = inputList[i].also {
             inputList[i] = inputList[0]
@@ -27,30 +27,36 @@ fun <T : Comparable<T>> heapSort(inputList: MutableList<T>) {
     }
 }
 
-private fun <T : Comparable<T>> heapify(inputList: MutableList<T>, heapSize: Int, i: Int) {
-
-    var largest = i //initialize largest as root
-    val leftChildldx = 2 * i + 1 // left = 2*i + 1
-    val rightChildldx = 2 * i + 2 // right = 2*i + 2
+/**
+ * It receives the inputList and converts it into a heap (thus called "heapify").
+ * a heap is a complete binary tree; which means all the levels of this binary tree other than last level
+ * are full of nodes. Furthermore, in a heap, all nodes are either >= (in a Max-Heap) or <=
+ * (in a Min-Heap) to their children.
+ *
+ * The "heapify()" function I've written here creates a Max-Heap (parent is more than or equal to its children).
+ */
+ fun <T : Comparable<T>> heapify(inputList: MutableList<T>, heapSize: Int, i: Int) {
+    var indexLargest = i //initialize largest as root
+    val indexLeftChild = 2 * i + 1 // left = 2*i + 1
+    val indexRightChild = 2 * i + 2 // right = 2*i + 2
 
     // if left child is larger than root
-    if (leftChildldx < heapSize && inputList[leftChildldx] > inputList[largest]) {
-        largest = leftChildldx
+    if (indexLeftChild < heapSize && inputList[indexLeftChild] > inputList[indexLargest]) {
+        indexLargest = indexLeftChild
     }
 
     // if right child is larger than root
-    if (rightChildldx < heapSize && inputList[rightChildldx] > inputList[largest]) {
-        largest = rightChildldx
+    if (indexRightChild < heapSize && inputList[indexRightChild] > inputList[indexLargest]) {
+        indexLargest = indexRightChild
     }
 
-    //if largest isn't root
-    if (largest != i) {
-        val swap = inputList[i]
-        inputList[i] = inputList[largest]
-        inputList[largest] = swap
+    // if indexLargest has been changed that means we need to swap the
+    // parent with one of its children (right child has the higher priority).
+    if (indexLargest != i) {
+        inputList[i] = inputList[indexLargest].also { inputList[indexLargest] = inputList[i] } // swapping
 
-        // recursive call to heapify the subtree
-        heapify(inputList, heapSize, largest)
+        // this recursive call is to make sure that this specific
+        // parent and its direct children comply with max-heap characteristics.
+        heapify(inputList, heapSize, indexLargest)
     }
-
 }
